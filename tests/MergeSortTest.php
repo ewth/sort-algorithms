914d2754bf0a6ba71633b00f7c0a2d91e898bc16
Ewan Thompson <ewanpt@gmail.com>
2020-05-14 21:43:38 +0800

    Tests for MergeSort class

---
+++ tests/MergeSortTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Sort;
+
+use PHPUnit\Framework\TestCase;
+
+class MergeSortTest extends TestCase
+{
+    public function testMergeSortSortsItems()
+    {
+        $items = [5, 3, 6, 1, 2, 4, 6, 9, 8, 1, 7, 1, 5];
+        $sort = new MergeSort;
+        $result = $sort->sort($items);
+        sort($items);
+        $this->assertSame($items, $result->getSorted());
+    }
+
+    public function testMergeSortReturnsSortTime()
+    {
+        $items = [5, 3, 6, 1, 2, 4, 6, 9, 8, 1, 7, 1, 5];
+        $sort = new MergeSort;
+        $result = $sort->sort($items);
+        $this->assertIsNumeric($result->getTimeTaken());
+    }
+
+    public function testMergeSortReturnsSwapCount()
+    {
+        $items = [5, 3, 6, 1, 2, 4, 6, 9, 8, 1, 7, 1, 5];
+        $sort = new MergeSort;
+        $result = $sort->sort($items);
+        $this->assertGreaterThanOrEqual(1, $result->getSwapsPerformed());
+    }
+}
