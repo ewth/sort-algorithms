f26c4d0210fad55982b70521b185c44919e2f48a
Ewan Thompson <ewanpt@gmail.com>
2020-05-15 19:30:59 +0800

    Alter insertion sort to use smaller list

--- src/demo.php
+++ src/demo.php
@@ -21,8 +21,10 @@ echo "Merge sort: {$sortedSwaps} swaps in {$sortedTime}ms\n";
 
 /**
  * Insertion sort
- * @todo: insertion sort is horribly inefficient with large lists; generate smaller list for this algo.
  */
+$data = \Sort\Data::randomSet(1000,5000);
+$itemCount = number_format(count($data));
+echo "Sorting {$itemCount} items...\n";
 $sortResults = $insertionSort->sort($data);
 $sortedTime = number_format($sortResults->getTimeTaken() * 1000);
 $sortedSwaps = number_format($sortResults->getSwapsPerformed());
