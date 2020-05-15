8858981adad35df633b2a0cddafa847a1e3a9b9b
Ewan Thompson <ewanpt@gmail.com>
2020-05-15 19:29:53 +0800

    Add test for ranged dataset

--- tests/DataTest.php
+++ tests/DataTest.php
@@ -12,10 +12,19 @@ class DataTest extends TestCase
         $this->assertIsArray($dataSet);
     }
 
-    public function testRandomSetReturnsAtLeast1000Items()
+    public function testRandomSetReturnsAtLeast1000ItemsWithNoParameters()
     {
         $dataSet = Data::randomSet();
         $this->assertGreaterThanOrEqual(1000, count($dataSet));
+    }
+
+    public function testRandomSetReturnsExpectedRangeOfItems()
+    {
+        $min = 106;
+        $max = 155;
+        $dataSet = Data::randomSet($min, $max);
+        $this->assertGreaterThanOrEqual($min, count($dataSet));
+        $this->assertLessThanOrEqual($max, count($dataSet));
     }
 
     public function testRandomSetReturnsOnlyIntegers()
