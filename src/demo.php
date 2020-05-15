856b17c52458a0c3c0a60724966d481177894374
Ewan Thompson <ewanpt@gmail.com>
2020-05-15 19:56:18 +0800

    Update timing information in demo script

--- src/demo.php
+++ src/demo.php
@@ -5,18 +5,21 @@ include('../vendor/autoload.php');
 $mergeSort = new \Sort\MergeSort();
 $insertionSort = new \Sort\InsertionSort();
 
-$data = \Sort\Data::randomSet();
-$itemCount = number_format(count($data));
+$timeTaken = new \Sort\TimeTaken();
 
-echo "Sorting {$itemCount} items...\n";
+$data = \Sort\Data::randomSet();
+
 
 /**
  * Merge sort
  */
+$itemCount = number_format(count($data));
+echo "Sorting {$itemCount} items...\n";
+$timeTaken->start();
 $sortResults = $mergeSort->sort($data);
-$sortedTime = number_format($sortResults->getTimeTaken() * 1000);
+$timeTaken->stop();
+$sortedTime = number_format($timeTaken->getTimeTaken() * 1000);
 $sortedSwaps = number_format($sortResults->getSwapsPerformed());
-
 echo "Merge sort: {$sortedSwaps} swaps in {$sortedTime}ms\n";
 
 /**
@@ -25,8 +28,9 @@ echo "Merge sort: {$sortedSwaps} swaps in {$sortedTime}ms\n";
 $data = \Sort\Data::randomSet(1000,5000);
 $itemCount = number_format(count($data));
 echo "Sorting {$itemCount} items...\n";
+$timeTaken->start();
 $sortResults = $insertionSort->sort($data);
-$sortedTime = number_format($sortResults->getTimeTaken() * 1000);
+$timeTaken->stop();
+$sortedTime = number_format($timeTaken->getTimeTaken() * 1000);
 $sortedSwaps = number_format($sortResults->getSwapsPerformed());
-
 echo "Insertion sort: {$sortedSwaps} swaps in {$sortedTime}ms\n";
