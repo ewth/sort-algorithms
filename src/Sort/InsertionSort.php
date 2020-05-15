3fbddfa1efc90c3288dddcf48673af743ea47bfa
Ewan Thompson <ewanpt@gmail.com>
2020-05-15 18:03:41 +0800

    Add insertion sort algorithm

---
+++ src/Sort/InsertionSort.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace Sort;
+
+/**
+ * Class InsertionSort
+ * @package Sort
+ */
+class InsertionSort implements SortInterface
+{
+
+    /**
+     * Sort using the insertion sort algorithm
+     *
+     * @param  array  $unsorted
+     * @return SortResults
+     */
+    public function sort(array $unsorted): SortResults
+    {
+        // Rudimentary time keeping
+        $timeStarted = microtime(true);
+
+        // Copy array
+        $sorted = $unsorted;
+
+        $swaps = 0;
+
+        // Iterate over array
+        for ($i = 0; $i < count($unsorted); $i++) {
+            // Store current item
+            $temp = $sorted[$i];
+
+            // Inner loop will iterate up to previous item
+            $j = $i - 1;
+            while ($j >= 0 && $sorted[$j] > $temp) {
+                // Swap items
+                $sorted[$j + 1] = $sorted[$j];
+                $swaps++;
+                $j--;
+            }
+            // Re-insert stored current item
+            $sorted[$j + 1] = $temp;
+        }
+
+        // Track time taken
+        $timeTaken = microtime(true) - $timeStarted;
+
+        // Return results
+        return new SortResults(
+            $sorted,
+            $timeTaken,
+            $swaps
+        );
+    }
+}
