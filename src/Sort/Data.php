16d0e6a99db1723e387c24c136ad82529fae04b8
Ewan Thompson <ewanpt@gmail.com>
2020-05-15 19:30:11 +0800

    Allow range of data items to be defined

--- src/Sort/Data.php
+++ src/Sort/Data.php
@@ -11,12 +11,14 @@ class Data
     /**
      * Generate an array of pseudo-random numbers
      *
+     * @param  int  $min
+     * @param  int  $max
      * @return array
      */
-    public static function randomSet() : array
+    public static function randomSet(int $min = 1000, int $max = 100000) : array
     {
         // Generate a series of random numbers that we can sort later
-        $numberOfItems = rand(1000,100000);
+        $numberOfItems = rand($min,$max);
         $items = [];
         for ($i = 0; $i < $numberOfItems; $i++) {
             $items[] = rand(0, 100);
